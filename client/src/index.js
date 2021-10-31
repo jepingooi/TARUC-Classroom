f9e12d576266308753bf0cfa8ef88f99249d2477
Asus <williamng0512@gmail.com>
2021-10-31 16:29:14 +0800

    removed unused file, added semantic ui css

--- client/src/index.js
+++ client/src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { render } from "react-dom";
 import { BrowserRouter } from "react-router-dom";
-
+import "semantic-ui-css/semantic.min.css";
 import "./index.css";
 import App from "./App";
 
