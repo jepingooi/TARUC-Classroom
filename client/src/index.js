9794531d0bab6a33b409452a37386b4a7df155d8
ooijp-pm18@student.tarc.edu.my <ooijp-pm18@student.tarc.edu.my>
2021-11-11 02:06:23 +0800

    ?

--- client/src/index.js
+++ client/src/index.js
@@ -2,7 +2,6 @@ import React from "react";
 import { render } from "react-dom";
 import { BrowserRouter } from "react-router-dom";
 import "semantic-ui-css/semantic.min.css";
-// import "bootstrap/dist/css/bootstrap.min.css";
 import "./styles.scss";
 import "react-toastify/dist/ReactToastify.css";
 import "./index.css";
