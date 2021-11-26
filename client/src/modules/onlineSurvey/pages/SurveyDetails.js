ab5b045883b36489d4adb71587202087540a337d
ooijp-pm18@student.tarc.edu.my <ooijp-pm18@student.tarc.edu.my>
2021-11-26 10:52:52 +0800

    add back button

--- client/src/modules/onlineSurvey/pages/SurveyDetails.js
+++ client/src/modules/onlineSurvey/pages/SurveyDetails.js
@@ -1,12 +1,13 @@
-import { useParams } from "react-router-dom";
-import { Container, Row, Col } from "react-bootstrap";
+import { useParams, useHistory } from "react-router-dom";
+import { Container, Row, Col, Button } from "react-bootstrap";
 import { getFirestore, doc, getDoc } from "firebase/firestore";
 import { firebaseConfig } from "../../../firebaseConfig.json";
 import { initializeApp } from "firebase/app";
 import { useState, useEffect, Fragment } from "react";
 import Chart from "../components/Chart";
 import ParagraphResponse from "../components/ParagraphResponse";
-
+import Heading from "../../../components/Heading.js";
+import Buttons from "../../../components/Buttons";
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
 
@@ -14,6 +15,7 @@ const SurveyDetails = () => {
   const [survey, setSurvey] = useState({});
   //same as req.params in express
   const { id } = useParams();
+  const history = useHistory();
 
   useEffect(async () => {
     const docRef = doc(db, "surveys", id);
@@ -28,16 +30,37 @@ const SurveyDetails = () => {
   }, []);
 
   return (
-    <Fragment>
-      {survey.questions &&
-        survey.questions.map((question, index) => {
-          if (question.type != "Paragraph") {
-            return <Chart question={question} key={index} />;
-          } else {
-            return <ParagraphResponse question={question} key={index} />;
-          }
-        })}
-    </Fragment>
+    <Container className="mt-3">
+      <Row className="d-flex align-items-center justify-content-center">
+        <Col md={6}>
+          <Heading>{survey.title}</Heading>
+        </Col>
+        <Col md={1} className="text-end">
+          <Button
+            variant="secondary"
+            size="lg"
+            onClick={() => {
+              history.goBack();
+            }}
+          >
+            Back
+          </Button>
+        </Col>
+      </Row>
+
+      <Row>
+        <Col>
+          {survey.questions &&
+            survey.questions.map((question, index) => {
+              if (question.type != "Paragraph") {
+                return <Chart question={question} key={index} />;
+              } else {
+                return <ParagraphResponse question={question} key={index} />;
+              }
+            })}
+        </Col>
+      </Row>
+    </Container>
   );
 };
 
