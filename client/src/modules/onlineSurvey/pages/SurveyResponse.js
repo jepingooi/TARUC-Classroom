caed546c145f53e009e0d5b6c9fcd670c4d14310
ooijp-pm18@student.tarc.edu.my <ooijp-pm18@student.tarc.edu.my>
2021-11-29 18:16:33 +0800

    fix edit survey bug

--- client/src/modules/onlineSurvey/pages/SurveyResponse.js
+++ client/src/modules/onlineSurvey/pages/SurveyResponse.js
@@ -52,11 +52,14 @@ const SurveyDetails = () => {
 
   return (
     <Fragment>
-      <Breadcrumbs
-        id={id}
-        active="response"
-        isDisabled={survey.status !== "Drafted"}
-      />
+      {survey.status && (
+        <Breadcrumbs
+          id={id}
+          active="response"
+          isDisabled={survey.status !== "Drafted"}
+        />
+      )}
+
       <Container className="mt-3">
         <Row className="d-flex align-items-center justify-content-center">
           <Col md={{ span: 6 }}>
